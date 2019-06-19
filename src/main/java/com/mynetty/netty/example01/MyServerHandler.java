cc61bec56307283dbe23dd211ba507bf47b35e46
GP017916 <624976130@qq.com>
2019-06-19 15:15:38 +0800

    Initial commit

---
+++ src/main/java/com/mynetty/netty/example01/MyServerHandler.java
@@ -0,0 +1,37 @@
+package com.mynetty.netty.example01;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.DataInputStream;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Slf4j
+public class MyServerHandler extends SimpleChannelInboundHandler<String> {
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+        log.info(ctx.channel().remoteAddress() + "server receive message：" + msg);
+
+        ctx.writeAndFlush("from server: "+"(" +msg+")" );
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        log.info("handlerAdded: " + ctx.channel().id().asLongText());
+    }
+
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+        log.info("handlerRemoved: " + ctx.channel().id().asLongText());
+    }
+
+}
